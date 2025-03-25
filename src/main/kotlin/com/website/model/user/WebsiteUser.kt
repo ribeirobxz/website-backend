0570c0f32def01e002b0772442b1e5d1c951bb3e
sn0wkzy <ribeirok427@gmail.com>
2025-03-25 13:10:17 -0300

    change group parameter to groups array in WebsiteUser.kt

--- src/main/kotlin/com/website/model/user/WebsiteUser.kt
+++ src/main/kotlin/com/website/model/user/WebsiteUser.kt
@@ -10,5 +10,29 @@ data class WebsiteUser(
     val uniqueId: @Contextual UUID = UUID.randomUUID(),
     val playerName: String,
     var passWord: String,
-    var group: Group
-)
+    var groups: Array<Group>
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as WebsiteUser
+
+        if (uniqueId != other.uniqueId) return false
+        if (playerName != other.playerName) return false
+        if (passWord != other.passWord) return false
+        if (!groups.contentEquals(other.groups)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = uniqueId.hashCode()
+        result = 31 * result + playerName.hashCode()
+        result = 31 * result + passWord.hashCode()
+        result = 31 * result + groups.contentHashCode()
+        return result
+    }
+
+}
